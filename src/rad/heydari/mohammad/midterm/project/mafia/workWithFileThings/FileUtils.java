2223b9ec38e14e58a7852f1fde9aed8269460556
mohammadrad81 <mohammad.rad81@gmail.com>
2021-06-11 10:34:16 +0430

    changing the file saving strategy ( in text instead of binary )

--- src/rad/heydari/mohammad/midterm/project/mafia/workWithFileThings/FileUtils.java
+++ src/rad/heydari/mohammad/midterm/project/mafia/workWithFileThings/FileUtils.java
@@ -3,13 +3,15 @@ package rad.heydari.mohammad.midterm.project.mafia.workWithFileThings;
 import rad.heydari.mohammad.midterm.project.mafia.chatThings.Message;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 public class FileUtils {
 
     private String fileName;
     private File file;
     public FileUtils(String userName){
-        fileName = userName + "Messages.bin";
+        fileName = userName + "Messages.txt";
         file = new File(fileName);
         if(file.exists()){
             file.delete();
@@ -25,8 +27,8 @@ public class FileUtils {
 
     public void saveMessage(Message message){
         synchronized (file){
-            try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(file , true))){
-                objectOutputStream.writeObject(message);
+            try (FileWriter fileWriter = new FileWriter(file , true)){
+                fileWriter.write(("// HISTORY MESSAGE :: " + message.getSenderName() + " : " + message.getMessageText() + "\n"));
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             } catch (IOException e) {
@@ -36,25 +38,13 @@ public class FileUtils {
     }
 
     public void printAllMessages(){
+
         synchronized (file){
-            try (ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(file))){
-                Message message = null;
-                while (true){
-                    try {
-                        message = (Message) objectInputStream.readObject();
-                        System.out.println("// HISTORY MESSAGE :: " +
-                                message.getSenderName() +
-                                " : " +
-                                message.getMessageText());
-                    }catch (EOFException e){
-                        break;
-                    }
+            try (Scanner scanner = new Scanner(file)){
+                while (scanner.hasNextLine()){
+                    System.out.println(scanner.nextLine());
                 }
             } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (ClassNotFoundException e) {
                 e.printStackTrace();
             }
         }
